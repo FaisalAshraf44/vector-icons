65e3dc1036bf4f161951eda953240ad409741da5
Amitt Mahajan <amitt.mahajan@gmail.com>
2020-08-31 22:03:14 +0200

    replace built js

--- build/createIconSetFromFontAwesome5.js
+++ build/createIconSetFromFontAwesome5.js
@@ -1,60 +1,115 @@
-import { Platform } from 'react-native';
+import createStackedIconSet from './createStackedIconSet';
+import createIconSetWithStyle from './createIconSetWithStyle';
 import createMultiStyleIconSet from './createMultiStyleIconSet';
-export const FA5Style = {
-    regular: 'regular',
-    light: 'light',
-    solid: 'solid',
-    brand: 'brand',
-    duotone: 'duotone'
+
+const FA5Style = {
+  regular: 'regular',
+  light: 'light',
+  solid: 'solid',
+  brand: 'brand',
+  duotone: 'duotone',
 };
-export function createFA5iconSet(glyphMap, metadata = {}, fonts, pro = false) {
-    const metadataKeys = Object.keys(metadata);
-    const fontFamily = `FontAwesome5${pro ? 'Pro' : 'Free'}`;
-    function fallbackFamily(glyph) {
-        for (let i = 0; i < metadataKeys.length; i += 1) {
-            const family = metadataKeys[i];
-            if (metadata[family].indexOf(glyph) !== -1) {
-                return family === 'brands' ? 'brand' : family;
-            }
-        }
-        return 'regular';
+
+function createFA5iconSet(glyphMap, metadata = {}, pro = false) {
+  const metadataKeys = Object.keys(metadata);
+  const fontFamily = `FontAwesome5${pro ? 'Pro' : 'Free'}`;
+
+  function fallbackFamily(glyph) {
+    for (let i = 0; i < metadataKeys.length; i += 1) {
+      const family = metadataKeys[i];
+      if (metadata[family].indexOf(glyph) !== -1) {
+        return family === 'brands' ? 'brand' : family;
+      }
     }
-    function glyphValidator(glyph, style) {
-        const family = style === 'brand' ? 'brands' : style;
-        if (metadataKeys.indexOf(family) === -1)
-            return false;
-        return metadata[family].indexOf(glyph) !== -1;
+
+    return 'regular';
+  }
+
+  function glyphValidator(glyph, style) {
+    const family = style === 'brand' ? 'brands' : style;
+    if (metadataKeys.indexOf(family) === -1) return false;
+    return metadata[family].indexOf(glyph) !== -1;
+  }
+
+  function createFontAwesomeIconSet(inputOptions) {
+    const { fontWeight, style, family, styleGlyphMap } = {
+      styleGlyphMap: glyphMap,
+      family: fontFamily,
+      ...inputOptions,
+    };
+
+    let styleName = style;
+    let fontFile = `FontAwesome5_${pro ? `Pro_${styleName}` : styleName}.ttf`;
+
+    if (styleName === 'Brands') {
+      fontFile = 'FontAwesome5_Brands.ttf';
+    } else if (styleName === 'Duotone') {
+      styleName = 'Solid';
+      fontFile = 'FontAwesome5_Duotone.ttf';
     }
-    function createFontAwesomeStyle(styleName, fontWeight, family = fontFamily) {
-        let fontFile = fonts[styleName];
-        return {
-            fontFamily: `${family}-${styleName}`,
-            fontFile,
-            fontStyle: Platform.select({
-                ios: {
-                    fontWeight,
-                },
-                default: {},
-            }),
-            glyphMap,
-        };
-    }
-    const brandIcons = createFontAwesomeStyle('Brand', '400');
-    const lightIcons = createFontAwesomeStyle('Light', '100');
-    const regularIcons = createFontAwesomeStyle('Regular', '400');
-    const solidIcons = createFontAwesomeStyle('Solid', '700');
-    const duotoneIcons = createFontAwesomeStyle('Duotone', '900');
-    const Icon = createMultiStyleIconSet({
-        brand: brandIcons,
-        light: lightIcons,
-        regular: regularIcons,
-        solid: solidIcons,
-        duotone: duotoneIcons,
-    }, {
-        defaultStyle: 'regular',
-        fallbackFamily,
-        glyphValidator,
+
+    return createIconSetWithStyle({
+      fontFamily: `${family}-${styleName}`,
+      fontFile,
+      fontStyle: Platform.select({
+        default: {},
+        ios: { fontWeight },
+      }),
+      glyphMap: styleGlyphMap,
     });
-    return Icon;
+  }
+
+  const brandIcons = createFontAwesomeIconSet({
+    style: 'Brands',
+    fontWeight: '400',
+    family: 'FontAwesome5Brands',
+  });
+  const lightIcons = createFontAwesomeIconSet({
+    style: 'Light',
+    fontWeight: '300',
+  });
+  const regularIcons = createFontAwesomeIconSet({
+    style: 'Regular',
+    fontWeight: '400',
+  });
+  const solidIcons = createFontAwesomeIconSet({
+    style: 'Solid',
+    fontWeight: '900',
+  });
+
+  const duotoneIcons = createStackedIconSet(
+    createFontAwesomeIconSet({
+      style: 'Duotone',
+      fontWeight: '900',
+      family: 'FontAwesome5Duotone',
+    }),
+    createFontAwesomeIconSet({
+      style: 'Duotone',
+      fontWeight: '900',
+      family: 'FontAwesome5Duotone',
+      styleGlyphMap: Object.keys(glyphMap).reduce((acc, key) => {
+        acc[key] = glyphMap[key] + 0x100000;
+        return acc;
+      }, {}),
+    })
+  );
+
+  const Icon = createMultiStyleIconSet(
+    {
+      brand: brandIcons,
+      light: lightIcons,
+      regular: regularIcons,
+      duotone: duotoneIcons,
+      solid: solidIcons,
+    },
+    {
+      defaultStyle: 'regular',
+      fallbackFamily,
+      glyphValidator,
+    },
+  );
+
+  return Icon;
 }
-//# sourceMappingURL=createIconSetFromFontAwesome5.js.map
+
+export { createFA5iconSet, FA5Style };
